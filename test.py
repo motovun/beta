182db82d9316440d9315512f89e4c6a4e0485c15
dev.motovun <dev.motovun@gmail.com>
2021-05-14 15:36:37 +0900

    no message

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ access = "52YSHipHLeMqYu8TOBZ6xTPqAUczAujch33iIJc3"          # 본인 값으로 
 secret = "nUPzqmSngcifHFxMFhqmWyCq2yPniJ74FUqVJZXR"          # 본인 값으로 변경
 upbit = pyupbit.Upbit(access, secret)
 
+
+
 print(upbit.get_balance("KRW-XRP"))     # KRW-XRP 조회
 print(upbit.get_balance("KRW"))         # 보유 현금 조회
 
